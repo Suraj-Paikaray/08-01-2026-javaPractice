651568df605bd011f1f1e0b945c636966c9812d2
Suraj Paikaray <pratapsuraj2226@gmail.com>
2026-01-16 23:49:55 +0530

    16/01/2026 java practice

---
+++ kidsWithCandies.java
@@ -0,0 +1,32 @@
+package javaPrograms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution { // <--- You need this "Container"
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        // 1. Find the current Champion (the max)
+        int maxCandies = 0;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > maxCandies) {
+                maxCandies = candies[i];
+            }
+        }
+
+        // 2. Create the result list
+        List<Boolean> result = new ArrayList<>();
+
+        // 3. The "What If" test
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
+    
