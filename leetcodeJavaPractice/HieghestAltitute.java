934285a0ab68313f655d254f68319c6b597d2604
Suraj Paikaray <pratapsuraj2226@gmail.com>
2026-01-22 23:47:39 +0530

    22/01/2026-java-practice

---
+++ leetcodeJavaPractice/HieghestAltitute.java
@@ -0,0 +1,14 @@
+package javaPrograms.leetcodeJavaPractice;
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        //-5,1,5,0,-7
+    int sum =0;
+        int res = 0;
+    for (int i = 0; i < gain.length; i++) {
+        sum = sum + gain[i];  // Add current gain
+        res = Math.max(res, sum);
+    }
+    return res;
+    }
+}
